72f269465187f59a509252ac6b7139a48d4a1fb2
kanaanzafar07 <kanaan.zafar@qaboolhai.com>
2021-02-14 22:31:00 +0500

    splash added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:zimble_hr/splash.dart';
+import 'package:zimble_hr/Auth/login.dart';
 
-
-Future<void> main() async {
-
+main() {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
 
   runApp(MaterialApp(
-    theme: ThemeData(
-        accentColor: Color(0xff7268C9),
-        fontFamily: 'Montserrat'
-    ),
+    theme: ThemeData(accentColor: Color(0xff7268C9), fontFamily: 'Montserrat'),
     debugShowCheckedModeBanner: false,
-    home: Splash(),
+    home: Login(),
   ));
 }
